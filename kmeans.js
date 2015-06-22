c1275de2b0d323fc40637255e82263a78d863df4
NathanEpstein <ne2210@columbia.edu>
2015-06-22 14:44:51 -0700

    update labels on each iteration

--- kmeans.js
+++ kmeans.js
@@ -1,9 +1,9 @@
-function kmeans(vectors, k, iterations) {
+function kmeans(points, k, iterations) {
   iterations = iterations || Math.pow(10,4);
 
   // intialize centroids randomly
   var centroids = [];
-  var bounds = getBounds(vectors);
+  var bounds = getBounds(points);
   for (var i = 0; i < k; i++) {
     centroids[i] = bounds.map(function(range, j) {
       return (Math.random() * (range.max - range.min) + range.min);
@@ -11,12 +11,16 @@ function kmeans(vectors, k, iterations) {
   };
 
   //until convergence
+  var c = points.map(function() { return 0 });
   for (var iter = 0; iter < iterations; iter++) {
-    // for each training vector
-    vectors.forEach(function(vector, i) {
-      // set c_i (label for vector i)
-      var minDistance = 0;
 
+    // for each training vector
+    points.forEach(function(x_i, i) {
+      // set c_i according to current mu_j (label for x_i)
+      var distancesSquared = centroids.map(function(mu_j) {
+        return sumOfSquareDiffs(x_i, mu_j);
+      });
+      c[i] = mindex(distancesSquared);
     });
 
     // for each of the k centroids
@@ -24,15 +28,15 @@ function kmeans(vectors, k, iterations) {
   };
 };
 
-function getBounds(vectors) {
-  var bounds = vectors[0].map(function(element) {
+function getBounds(points) {
+  var bounds = points[0].map(function(element) {
     return {min: element, max: element};
   });
 
-  vectors.forEach(function(vector) {
-    vector.forEach(function(element, i) {
-      if (element < bounds[i].min) bounds[i].min = element;
-      if (element > bounds[i].max) bounds[i].max = element;
+  points.forEach(function(x_i) {
+    x_i.forEach(function(element, j) {
+      if (element < bounds[j].min) bounds[j].min = element;
+      if (element > bounds[j].max) bounds[j].max = element;
     });
   });
   return bounds;
@@ -43,6 +47,13 @@ function sumOfSquareDiffs(oneVector, anotherVector) {
     return Math.pow(element - anotherVector[i], 2);
   });
   return Math.pow(squareDiffs.reduce(function(a, b) { return a + b }, 0), 0.5);
+};
+
+function mindex(array) {
+  var min = array.reduce(function(a, b) {
+    return Math.min(a, b);
+  });
+  return array.indexOf(min);
 };
 
 
