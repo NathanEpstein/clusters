f7f06ea871dda61c118086ae52a7d2ce0e857607
NathanEpstein <ne2210@columbia.edu>
2015-06-22 12:03:03 -0700

    implement sum of square diffs for cluster centroid assignment

--- kmeans.js
+++ kmeans.js
@@ -1,9 +1,9 @@
-function kmeans(data, k, iterations) {
+function kmeans(vectors, k, iterations) {
   iterations = iterations || Math.pow(10,4);
 
   // intialize centroids randomly
   var centroids = [];
-  var bounds = getBounds(data);
+  var bounds = getBounds(vectors);
   for (var i = 0; i < k; i++) {
     centroids[i] = bounds.map(function(range, j) {
       return (Math.random() * (range.max - range.min) + range.min);
@@ -11,27 +11,38 @@ function kmeans(data, k, iterations) {
   };
 
   //until convergence
+  for (var iter = 0; iter < iterations; iter++) {
+    // for each training vector
+    vectors.forEach(function(vector, i) {
+      // set c_i (label for vector i)
+      var minDistance = 0;
 
-    // for each training point
-      // set c_i (label for point i)
-    data.forEach(function(vector, ))
+    });
 
     // for each of the k centroids
       // set the location of the centroid using the label locations.
+  };
 };
 
-function getBounds(data) {
-  var bounds = data[0].map(function(value) {
-    return {min: value, max: value};
+function getBounds(vectors) {
+  var bounds = vectors[0].map(function(element) {
+    return {min: element, max: element};
   });
 
-  data.forEach(function(array) {
-    array.forEach(function(datum, i) {
-      if (datum < bounds[i].min) bounds[i].min = datum;
-      if (datum > bounds[i].max) bounds[i].max = datum;
+  vectors.forEach(function(vector) {
+    vector.forEach(function(element, i) {
+      if (element < bounds[i].min) bounds[i].min = element;
+      if (element > bounds[i].max) bounds[i].max = element;
     });
   });
   return bounds;
+};
+
+function sumOfSquareDiffs(oneVector, anotherVector) {
+  var squareDiffs = oneVector.map(function(element,i) {
+    return Math.pow(element - anotherVector[i], 2);
+  });
+  return Math.pow(squareDiffs.reduce(function(a, b) { return a + b }, 0), 0.5);
 };
 
 
